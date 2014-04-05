12da5a90c4e57bdc87485caa30ea7ee789d2b85e
Casey Anderson <casey.anderson@se-eng.com>
2014-04-05 11:34:17 -0600

    SoundPlayer class added.

---
+++ ConsoleApplication4/SoundPlayer.h
@@ -0,0 +1,20 @@
+#pragma once
+/*
+#include <dshow.h>
+#include <string>
+
+class SoundPlayer
+{
+public:
+	SoundPlayer(void);
+	~SoundPlayer(void);
+	int playSound(std::string filename);
+
+private:
+	IGraphBuilder *pGraph;
+    IMediaControl *pControl;
+    IMediaEvent   *pEvent;
+	HRESULT hr;
+};
+
+*/
